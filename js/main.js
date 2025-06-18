5df637a58c65e1c734e4a719d3d5c3c1e394d97c
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-06-19 01:07:09 +0600

    buton 1,2,3 dom selected

--- js/main.js
+++ js/main.js
@@ -6,6 +6,10 @@
 
 let classs = document.getElementById('classs')
 
+let one = document.getElementById('one')
+let two = document.getElementById('two')
+let three = document.getElementById('three')
+
 let four = document.getElementById('four')
 let five = document.getElementById('five')
 let six = document.getElementById('six')
@@ -13,6 +17,10 @@ let six = document.getElementById('six')
 let seven = document.getElementById('seven')
 let eight = document.getElementById('eight')
 let nine = document.getElementById('nine')
+
+
+
+
 
 
 
