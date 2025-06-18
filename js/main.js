85b8c3d9bcdc98b8aa386d6532e4f45b7a2021f3
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-06-19 00:57:22 +0600

    in js set button

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
+// let removeLastChar=()=>{
+//        let input = document.getElementById("input");
+//     input.value = input.value.slice(0, -1);
+// }
 
 
-let removeLastChar=()=>{
-       let input = document.getElementById("input");
-    input.value = input.value.slice(0, -1);
-}
+let classs = document.getElementById('classs')
+let seven = document.getElementById('seven')
+let eight = document.getElementById('eight')
+let nine = document.getElementById('nine')
+
+
+seven.addEventListener('click', () => {
+    classs.value += 7
+    
+})
+
