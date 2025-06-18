3b84f97da07ca67995aaf7558ce91f593a790db9
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-06-18 21:39:09 +0600

    7 button added

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let seven = document.querySelector('.seven')
+let input = document.querySelector('.input')
+
+
+seven.addEventListener('click',
+    () => {
+   
+
+        input.value += 7
+    }
+)
