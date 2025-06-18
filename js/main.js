bbccebafd3d6a20f7c10e905bce32dcb3e63f8cd
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-06-18 22:54:50 +0600

    basic js done

--- js/main.js
+++ js/main.js
@@ -1,11 +1,52 @@
-let seven = document.querySelector('.seven')
-let input = document.querySelector('.input')
+// let seven = document.querySelector('.seven')
+// let input = document.querySelector('.input')
+// let eight = document.querySelector('.eight')
+// let gun = document.querySelector('.gun')
+// let vag = document.querySelector('.vag')
+// let jog = document.querySelector('.jog')
+// let biyog = document.querySelector('.biyog')
+// let equal = document.querySelector('.equal')
+// // let input = document.querySelector('.input')
 
 
-seven.addEventListener('click',
-    () => {
-   
+// seven.addEventListener('click',
+//     () => {
 
-        input.value += 7
-    }
-)
+
+//         input.value += 7
+//     }
+// )
+
+// eight.addEventListener('click',
+//     () => {
+
+
+//         input.value += 8
+//     }
+// )
+
+
+// jog.addEventListener('click',
+//     () => {
+
+
+//         input.value += +
+//     }
+// )
+
+
+
+
+
+
+
+
+
+
+// equal.addEventListener('click',
+//     () => {
+
+
+//         input.value += eval(input.value)
+//     }
+// )
