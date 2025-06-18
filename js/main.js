b8b71836d720427a086d09b0b64d8c05d4286907
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-06-18 23:21:46 +0600

    calculator is working but delet buton is not working

--- js/main.js
+++ js/main.js
@@ -1,52 +1,6 @@
-// let seven = document.querySelector('.seven')
-// let input = document.querySelector('.input')
-// let eight = document.querySelector('.eight')
-// let gun = document.querySelector('.gun')
-// let vag = document.querySelector('.vag')
-// let jog = document.querySelector('.jog')
-// let biyog = document.querySelector('.biyog')
-// let equal = document.querySelector('.equal')
-// // let input = document.querySelector('.input')
 
 
-// seven.addEventListener('click',
-//     () => {
-
-
-//         input.value += 7
-//     }
-// )
-
-// eight.addEventListener('click',
-//     () => {
-
-
-//         input.value += 8
-//     }
-// )
-
-
-// jog.addEventListener('click',
-//     () => {
-
-
-//         input.value += +
-//     }
-// )
-
-
-
-
-
-
-
-
-
-
-// equal.addEventListener('click',
-//     () => {
-
-
-//         input.value += eval(input.value)
-//     }
-// )
+let removeLastChar=()=>{
+       let input = document.getElementById("input");
+    input.value = input.value.slice(0, -1);
+}
